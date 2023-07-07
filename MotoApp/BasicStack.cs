50f7d4cb8d2aa2ceb05057cef4824df9876e4172
TOMASZ SZLETTER <T.SZLETTER@GMAIL.COM>
2023-07-07 21:03:51 +0200

    Dodaj pliki projektów.

---
+++ MotoApp/BasicStack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoApp
+{
+    internal class BasicStack<T>
+    {
+        private readonly T[] _items;
+
+        private int _currentIndex = -1;
+        public BasicStack() => _items = new T[10];
+
+        public int Count => _currentIndex + 1;
+
+        public void Push(T item) =>_items[++_currentIndex] = item;
+        public T Pop() => _items[_currentIndex--];
+    }
+}
---
+++ MotoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using MotoApp;
+var stack = new BasicStack<double>();
+
+stack.Push(item: 4.5);
+stack.Push(item: 43);
+stack.Push(item: 333);
+
+var stackString = new BasicStack<string>();
+stackString.Push(item: "Company");
+stackString.Push(item: "Company");
+stackString.Push(item: "Company");
+
+
+double sum = 0.0;
+
+while (stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+    sum += item;
+}
+
+Console.WriteLine($"Sum: {sum}");
